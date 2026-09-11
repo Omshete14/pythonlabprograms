5942323604d12c8d12a90942cfd0ce0a01451c08
Omshete14 <omshete1409@gmail.com>
2026-09-11 14:39:37 +0530

    Create script to reverse a given number
    Add a script to reverse a given number.

---
+++ to reverse a given number.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reversed number is:", reverse)
+You’re using our basic model. Log in or sign up for more intelligence.
+Log in
+Sign up for free
